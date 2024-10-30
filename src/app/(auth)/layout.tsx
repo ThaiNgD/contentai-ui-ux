5468357184c07deab154abb7222850885bd4aed9
Draxxxx <thaind19052001@gmail.com>
2024-10-30 09:45:10 +0700

    Add some custom field and tan-query env setup

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,20 @@
+import BackGroundImage from "@/assets/images/banner-load.png";
+import Image from "next/image";
 export default function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className={` flex items-center justify-center bg-black h-screen`}>
-      <div className=" rounded-lg w-[400px] h-[600px] bg-white">{children}</div>
+    <div
+      className={`px-[30px] flex items-center justify-between bg-white h-screen`}
+    >
+      <Image src={BackGroundImage} className="w-[50%]" alt="Background Image" />
+      <div className="w-[50%] flex items-center justify-center">
+        <div className="border-2 shadow-xl rounded-lg w-[500px] h-[800px] bg-white">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CustomField/InputField.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { cn } from "@/helper/utils";
+import { FormikProps } from "formik";
+import { get } from "lodash";
+import { InputHTMLAttributes } from "react";
+import { IoClose } from "react-icons/io5";
+import ErrorHelperText from "./ErrorHelperText";
+import WrapperLabelForm, { WrapperLabelFormProps } from "./WrapperLabelForm";
+
+export interface IFieldFormCustom<T> {
+  name: string;
+  formik?: FormikProps<T>;
+  msgError?: string;
+}
+
+export interface InputFieldProps<T>
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, "name">,
+    Omit<WrapperLabelFormProps, "children">,
+    IFieldFormCustom<T> {
+  hiddenError?: boolean;
+  title?: string;
+  clsTitle?: string;
+  clsLabelWrapper?: string;
+  hiddenBgDisabled?: boolean;
+  isClear?: boolean;
+  onClear?: () => void;
+}
+
+const InputField = <T,>({
+  formik,
+  name,
+  label,
+  isRequired,
+  classWapper,
+  isVertical,
+  msgError,
+  hiddenError,
+  clsLabelWrapper,
+  isClear,
+  onClear,
+  ...spread
+}: InputFieldProps<T>): JSX.Element => {
+  const newValue = formik ? get(formik.values, name) : spread.value;
+  const isShowClear = newValue && isClear;
+
+  return (
+    <WrapperLabelForm
+      isRequired={isRequired}
+      label={label}
+      classWapper={cn(classWapper, isShowClear && "[&>div]:relative")}
+      isVertical={isVertical}
+      clsLabelWrapper={clsLabelWrapper}
+    >
+      <input
+        autoComplete="off"
+        {...spread}
+        name={name}
+        id={name}
+        onChange={formik ? formik.handleChange : spread?.onChange}
+        onBlur={formik ? formik.handleBlur : spread?.onBlur}
+        value={newValue}
+        className={cn(
+          "form-input w-full pr-3 !outline-none focus:border-slate-300",
+          spread?.className,
+          spread?.disabled || spread?.readOnly
+            ? "!cursor-default"
+            : "!cursor-text",
+          spread?.disabled && "bg-gray-100",
+          !!isShowClear && "pr-6"
+        )}
+      />
+
+      {!!isShowClear && (
+        <IoClose
+          className="absolute right-1 top-0 translate-y-1/2 cursor-pointer select-none text-[#cccccc]"
+          onClick={onClear}
+          size={20}
+        />
+      )}
+
+      <ErrorHelperText
+        isShow={
+          (formik &&
+            !hiddenError &&
+            get(formik?.touched, name) &&
+            get(formik?.errors, name)) ||
+          msgError
+        }
+        msgError={msgError ? msgError : get(formik?.errors, name)}
+      />
+    </WrapperLabelForm>
+  );
+};
+
+export default InputField;
