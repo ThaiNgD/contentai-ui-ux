8c56772d5690e161039003c45123d540190b5c7d
Draxxxx <thaind19052001@gmail.com>
2024-11-23 12:03:56 +0700

    Add fix ai-writer

--- src/app/[locale]/(home)/ai-writer/google-ad-title/_lib/InputContent.tsx
+++ src/app/[locale]/(home)/ai-writer/google-ad-title/_lib/InputContent.tsx
@@ -4,16 +4,18 @@ import { SelectField } from "@/components/CustomField/SelectField";
 import TextAreaField from "@/components/CustomField/TextAreaField";
 import configLanguageSelector from "@/config/configLanguageSelector";
 import configModuleSelector from "@/config/configModule";
-import { aiGoogleAdsHeadline } from "@/service/axios/AIWriterApi";
+import { cn } from "@/helper/function";
+import { useAiMetaDescriptionSeo } from "@/service/aiwriter/aiblog_v2/useAiMetaDescriptionSeo";
 import { Button } from "flowbite-react";
 import { useFormik } from "formik";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 
 interface InputProps {
   setCkData: Dispatch<SetStateAction<string>>;
 }
 
 const InputContent = ({ setCkData }: InputProps) => {
+  const { data, mutate, isPending } = useAiMetaDescriptionSeo();
   const formik = useFormik<IFormGoogleAdsHeading>({
     initialValues: {
       brandName: "",
@@ -23,13 +25,14 @@ const InputContent = ({ setCkData }: InputProps) => {
       language: "",
     },
     onSubmit: async (values) => {
-      await aiGoogleAdsHeadline.create(values).then((values) => {
-        setCkData(values.result);
-        // Handle form submission
-        console.log(values);
-      });
+      mutate(values);
     },
   });
+  useEffect(() => {
+    if (data && data.result) {
+      setCkData(data.result);
+    }
+  }, [data]);
   return (
     <form
       id="form-submit"
@@ -44,14 +47,6 @@ const InputContent = ({ setCkData }: InputProps) => {
         className="h-[50px] !bg-[#F5F9FC] shadow-inner"
         formik={formik}
       />
-      <TextAreaField
-        name={"description"}
-        placeholder="Mô tả sản phẩm/dịch vụ"
-        title="Mô tả sản phẩm/dịch vụ"
-        clsTitle="font-bold mb-1 italic"
-        className="h-[150px] !bg-[#F5F9FC] shadow-inner"
-        formik={formik}
-      />
       <InputField
         name={"keyword"}
         placeholder="Từ khóa"
@@ -60,7 +55,14 @@ const InputContent = ({ setCkData }: InputProps) => {
         className="h-[50px] !bg-[#F5F9FC] shadow-inner"
         formik={formik}
       />
-
+      <TextAreaField
+        name={"description"}
+        placeholder="Mô tả sản phẩm/dịch vụ"
+        title="Mô tả sản phẩm/dịch vụ"
+        clsTitle="font-bold mb-1 italic"
+        className="h-[150px] min-h-[50px] max-h-[400px] !bg-[#F5F9FC] shadow-inner"
+        formik={formik}
+      />
       <SelectField
         clsLabelWrapper="font-bold italic"
         label="Model"
@@ -78,9 +80,16 @@ const InputContent = ({ setCkData }: InputProps) => {
       <Button
         form="form-submit"
         type="submit"
-        className="bg-blue-500 mt-[30px] shadow-lg duration-200 rounded-full hover:shadow-none hover:translate-y-0.5"
+        className={cn(
+          "bg-blue-500 mt-[30px] shadow-lg duration-200 rounded-full hover:shadow-none hover:translate-y-0.5",
+          isPending && "pointer-events-none"
+        )}
       >
-        Tạo nội dung
+        {isPending ? (
+          <div className="loading size-[24px]"></div>
+        ) : (
+          "Tạo nội dung"
+        )}
       </Button>
     </form>
   );
