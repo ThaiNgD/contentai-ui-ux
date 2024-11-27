70a01259e34b6ef7f3608d86c816a31e7cb521af
Draxxxx <thaind19052001@gmail.com>
2024-11-27 16:02:23 +0700

    Add Fixed

--- src/components/ContentComponent.tsx/FolderDisplayComponent.tsx
+++ src/components/ContentComponent.tsx/FolderDisplayComponent.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { cn } from "@/helper/utils";
+import { useGetPathComponent } from "@/hook/useGetPathComponent";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FaFolderOpen } from "react-icons/fa";
 import { HiOutlineDotsVertical } from "react-icons/hi";
@@ -13,11 +15,13 @@ const FolderDisplayComponent = ({
   time,
   clsContainerWidth,
 }: FolderDisplayComponentProps) => {
+  const router = useRouter();
+  const { pathWithoutLocale } = useGetPathComponent();
   const [isSelected, setIsSelected] = useState(false);
   return (
     <div
       className={cn(
-        "rounded-xl hover:shadow-lg hover:-translate-y-0.5 duration-100 px-4 py-3 items-center bg-gray-200 grid w-[420px] ",
+        "rounded-xl hover:shadow-lg hover:-translate-y-0.5 duration-100 px-4 py-3 items-center bg-gray-200 grid w-[full] ",
         clsContainerWidth,
         isSelected && "bg-yellow-400 text-white"
       )}
@@ -26,7 +30,10 @@ const FolderDisplayComponent = ({
         gap: "10px",
       }}
       role="button"
-      onClick={(): void => setIsSelected(true)}
+      onClick={(): void => {
+        setIsSelected(true);
+        router.push(`${pathWithoutLocale}/${1}`);
+      }}
     >
       <FaFolderOpen
         size={30}
