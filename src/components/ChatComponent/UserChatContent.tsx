ff9277d559232997df9aae59cf3b38bf104517b6
Draxxxx <thaind19052001@gmail.com>
2024-12-02 12:03:13 +0700

    Fix Chat Container

--- src/components/ChatComponent/UserChatContent.tsx
+++ src/components/ChatComponent/UserChatContent.tsx
@@ -62,6 +62,66 @@ const UserChatContent = ({
           <div className="w-fit max-w-[80%] relative rounded-xl bg-gray-100">
             <p className="text-sm text-cursor whitespace-normal typewriter-animation w-fit p-2 text-gray-500 dark:text-gray-400">
               {message}
+              {/* <svg
+                className="w-[20px]"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 200 200"
+              >
+                <circle
+                  fill="#BFCCD2"
+                  stroke="#BFCCD2"
+                  stroke-width="15"
+                  r="15"
+                  cx="40"
+                  cy="100"
+                >
+                  <animate
+                    attributeName="opacity"
+                    calcMode="spline"
+                    dur="2"
+                    values="1;0;1;"
+                    keySplines=".5 0 .5 1;.5 0 .5 1"
+                    repeatCount="indefinite"
+                    begin="-.4"
+                  ></animate>
+                </circle>
+                <circle
+                  fill="#BFCCD2"
+                  stroke="#BFCCD2"
+                  stroke-width="15"
+                  r="15"
+                  cx="100"
+                  cy="100"
+                >
+                  <animate
+                    attributeName="opacity"
+                    calcMode="spline"
+                    dur="2"
+                    values="1;0;1;"
+                    keySplines=".5 0 .5 1;.5 0 .5 1"
+                    repeatCount="indefinite"
+                    begin="-.2"
+                  ></animate>
+                </circle>
+                <circle
+                  fill="#BFCCD2"
+                  stroke="#BFCCD2"
+                  stroke-width="15"
+                  r="15"
+                  cx="160"
+                  cy="100"
+                >
+                  <animate
+                    attributeName="opacity"
+                    calcMode="spline"
+                    dur="2"
+                    values="1;0;1;"
+                    keySplines=".5 0 .5 1;.5 0 .5 1"
+                    repeatCount="indefinite"
+                    begin="0"
+                  ></animate>
+                </circle>
+              </svg> */}
             </p>
             <div
               role="button"
