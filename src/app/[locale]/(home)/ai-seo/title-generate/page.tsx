2e8448c85cd3bfe029ea963c30d5557b80a594f0
Draxxxx <thaind19052001@gmail.com>
2024-12-14 12:02:20 +0700

    Add fix layout

--- src/app/[locale]/(home)/ai-seo/title-generate/page.tsx
+++ src/app/[locale]/(home)/ai-seo/title-generate/page.tsx
@@ -15,7 +15,7 @@ const Page = () => {
       />
       <div className="grid grid-cols-[35%,calc(65%-30px)] justify-between flex-auto bg-[#F5F9FC] px-[40px] py-[40px] gap-[30px] ">
         <InputContent setCkData={setCkData} />
-        <div className="bg-white flex flex-col gap-2 shadow-lg  p-[32px] rounded-xl">
+        <div className="bg-white flex flex-col gap-2 shadow-lg p-[32px] rounded-xl">
           <SaveContentComponent />
           <CustomEditor data={ckData} />
         </div>
